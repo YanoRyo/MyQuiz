ea14dac17e540d460802ece42bea4d3b9ed72fe1
YanoRyo <yanoryo7032@gmail.com>
2022-08-14 11:57:12 +0900

    質問正誤機能

--- app/src/main/java/com/example/myquiz/Constants.kt
+++ app/src/main/java/com/example/myquiz/Constants.kt
@@ -3,6 +3,11 @@ package com.example.myquiz
 object Constants {
 
 
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS:String = "total_questions"
+    const val CORRECT_ANSWERS:String = "correct_answers"
+
+
     fun getQuestions(): ArrayList<Question> {
         val questionsList = ArrayList<Question>()
 
